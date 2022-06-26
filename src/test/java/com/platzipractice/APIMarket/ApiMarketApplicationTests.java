7b5e91094b83354d2cc559a88db9fd0c74f5765b
Leonardo Andrés Gayón Espinel <leonardo.andresin@gmail.com>
2022-06-26 16:55:24 -0500

    configuración inicial

---
+++ src/test/java/com/platzipractice/APIMarket/ApiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzipractice.APIMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
